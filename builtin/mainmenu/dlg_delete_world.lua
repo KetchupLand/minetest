40c41fa8d67d062b43698391d34fc56ca4d034fc
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-24 19:07:31 +0200

    Rewrite the dlg_delete_world formspec code.

--- builtin/mainmenu/dlg_delete_world.lua
+++ builtin/mainmenu/dlg_delete_world.lua
@@ -17,14 +17,22 @@
 
 
 local function delete_world_formspec(dialogdata)
-	local retval =
-		"size[10,2.5,true]" ..
-		"label[0.5,0.5;" ..
-		fgettext("Delete World \"$1\"?", dialogdata.delete_name) .. "]" ..
-		"style[world_delete_confirm;bgcolor=red]" ..
-		"button[0.5,1.5;2.5,0.5;world_delete_confirm;" .. fgettext("Delete") .. "]" ..
-		"button[7.0,1.5;2.5,0.5;world_delete_cancel;" .. fgettext("Cancel") .. "]"
-	return retval
+	return formspec_wrapper([[
+		formspec_version[4]
+		size[10,4,true]
+		style[message;border=false;textcolor=#000000]
+		button[0,1;10,1;message;${msg}]
+		${common_styling}
+
+		style[world_delete_confirm;bgcolor=#ff3333]
+		button[1.5,3;2.5,0.8;world_delete_confirm;${txt_delete}]
+		button[6,3;2.5,0.8;world_delete_cancel;${txt_cancel}]
+	]], {
+		common_styling = kl_formspec_styling(),
+		msg = fgettext("Delete World \"$1\"?", dialogdata.delete_name),
+		txt_delete = fgettext('Delete'),
+		txt_cancel = fgettext('Cancel')
+	})
 end
 
 local function delete_world_buttonhandler(this, fields)
